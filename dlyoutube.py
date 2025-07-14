4bc64605f4571aa5e5152d060dec76786522d699
Passakon Panseng <162565700+pnackp@users.noreply.github.com>
2025-07-14 13:42:54 +0700

    Update dlyoutube.py

--- dlyoutube.py
+++ dlyoutube.py
@@ -3,8 +3,8 @@
 import yt_dlp #import lib 
 def download_youtube_video(url, save_path="."):
     ydl_opts = {
-        'outtmpl': f'{save_path}/%(title)s.%(ext)s',
-        'format': 'best'
+        'outtmpl': f'{save_path}/%(title)s.%(ext)s', # save path is where you need to intall at , title is title video . ext is extension file
+        'format': 'best' #set download highest quality
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
